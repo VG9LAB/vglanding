1293698cbe7ac6db282350ae9d1c481b6fb78d1d
Chang Liu <bobbyliu117@gmail.com>
2022-01-09 20:41:41 -0800

    x

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -2,6 +2,7 @@ const {resolve} = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 const { ModuleFederationPlugin } = require('webpack').container;
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 const packageJson = require('../package.json');
 
 const plugins = [
@@ -12,6 +13,9 @@ const plugins = [
       './Landing': './src/bootstrap',
     },
     shared: packageJson.dependencies,
+  }),
+  new HTMLWebpackPlugin({
+    template: 'public/index.html'
   }),
   new CleanWebpackPlugin(),
 ];
@@ -26,12 +30,23 @@ module.exports = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    path: resolve(__dirname, '..', 'build'),
+    path: resolve(__dirname, '..', 'docs'),
     // publicPath: 'http://192.168.254.12:4011/',
   },
   plugins,
   module: {
     rules: [tsRule],
+  },
+  devServer: {
+    port: 4011,
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
   },
   // externalsPresets: { node: true }, // in order to ignore built-in modules like path, fs, etc.
   // externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
